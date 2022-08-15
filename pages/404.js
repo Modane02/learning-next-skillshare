526187dd33c43e964fe30b11a3e55bfb08f80304
Modane <danielnegri2002@gmail.com>
2022-08-15 16:51:16 -0300

    Idem anterior, só que agora mais certo

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+const errorPage = () => {
+    const route = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            route.push("/");
+        }, 2000)
+    }, []);
+    return ( 
+        <h1>Page Not Found</h1>
+     );
+}
+ 
+export default errorPage;
